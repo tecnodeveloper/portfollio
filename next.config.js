e240e0f9aa9eea4addb94432548120314f0eb0b1
tecnodeveloper <recluzedev@gmail.com>
2026-01-08 07:18:11 +0000

    Configure Next.js for github Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	output: 'export',
 	images: {
 		unoptimized: true,
 	},
+	trailingSlash: true,
 	webpack: (config) => {
 		config.resolve.extensions.push(".ts", ".tsx");
 		return config;
